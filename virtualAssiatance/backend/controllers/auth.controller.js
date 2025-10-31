a7238de3be2b3b18465cc19809664e2355343285
Palak Gupta <guptapalakpg9756@gmail.com>
2025-10-31 17:04:14 +0530

    Updated backend routes and added frontend files

--- virtualAssiatance/backend/controllers/auth.controller.js
+++ virtualAssiatance/backend/controllers/auth.controller.js
@@ -17,11 +17,12 @@ export const signUp=async (req,res)=>{
 
         const hashedPassword=await bcrypt.hash(password,10)
 
-        const user = await User.create({
+         const user = await User.create({
             name,
             password:hashedPassword,
             email
         })
+
 
         const token = await genToken(user._id)
 
@@ -43,22 +44,18 @@ export const Login=async (req,res)=>{
     try {
         const {email,password}=req.body;
 
-        const exitEmail=await User.findOne({email})
-        if(exitEmail){
-            return res.status(400).json({message:"email already exits !"})
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({message:"email does not exits !"})
         }
 
-        if(password.length<6){
-            return res.status(400).json({message:"password must be at least 6 character !"})
-        }
+        const isMatch=await bcrypt.compare(password,user.password)
 
+        if(!isMatch){
+             return res.status(400).json({message:"incorrect password !"})
+        }
+        
         const hashedPassword=await bcrypt.hash(password,10)
-
-        const user = await User.create({
-            name,
-            password:hashedPassword,
-            email
-        })
 
         const token = await genToken(user._id)
 
@@ -69,9 +66,18 @@ export const Login=async (req,res)=>{
             secure:false
         })
 
-        return res.status(201).json(user)
+        return res.status(200).json(user)
 
     } catch (error) {
-        return res.status(500).json({message:`sign up error ${error}`})
+        return res.status(500).json({message:`Login error ${error}`})
     }
 }
+
+export const logout=async(req,res)=>{
+    try {
+        res.clearCookie("token")
+        return res.status(200).json({message:"Logout successfully"})
+    } catch (error) {
+        return res.status(500).json({message:`Logout error ${error}`})
+    }
+    }
---
+++ virtualAssiatance/backend/routes/auth.routes.js
@@ -0,0 +1,14 @@
+import express from "express"
+import { Login, logout, signUp } from "../controllers/auth.controller.js"
+
+const authRouter= express.Router()
+
+authRouter.post("/signup",signUp)
+
+authRouter.post("/signin",Login)
+
+authRouter.get("/logout",logout)
+
+export default authRouter
+
+
